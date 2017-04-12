82f3adabb10d34e483a6830ff1d8af83eeae2285
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2017-04-12 05:55:40 +0100

    Completed 10755
    Tried memorization, but it just turned out that Java was slow and I didn't need BigInts.
    See Java code

--- UVA-Practice/10755.c
+++ UVA-Practice/10755.c
@@ -3,21 +3,29 @@
 
 
 int main() {
+    // Basic initialization
     int test_cases;
     scanf("%d", &test_cases);
 
-    for(int i = 0; i<test_cases; i++) {
+
+    while(test_cases--) {
+
+        // Get the dimensions of the array
         int A, B, C;
         scanf("%d%d%d", &A, &B, &C);
 
+        // Need a long long arr to store all the large values
         long long arr[A][B][C];
 
+        // Basic initialization loop - tried doing something with memorization and longest running sum but didn't work.
         for(int i = 0; i<A; i++) {
             for(int j = 0; j<B; j++) {
                 for(int k = 0; k<C; k++) {
-                    int val;
-                    scanf("%d", &val);
+                    // Get the input.
+                    long long val;
+                    scanf("%lld", &val);
 
+                    // Calculate the sum from 0,0 to that point
                     arr[i][j][k] = val
                     + (j ? arr[i][j-1][k] : 0)
                     + (k ? arr[i][j][k-1] : 0)
@@ -30,18 +38,20 @@ int main() {
                 }
             }
         }
-
+        
+        // Long long to get the max sum-sum
         long long max_sum = arr[0][0][0];
+        
+        // I'm sure there's a way to do this better than n^6 with something like memorization, but I haven't found it - see Java code for attempt.
         // O(n^6)... but the max of n is 20 ~=~ 2^4, thus 2^24? 2^10 = 1024, so 1024^2? Ouch.
         for(int start_i = 0; start_i<A; start_i++) {
             for(int start_j = 0; start_j<B; start_j++) {
                 for(int start_k = 0; start_k < C; start_k++) {
-
-
                         for(int end_i = start_i; end_i<A; end_i++) {
                             for(int end_j = start_j; end_j<B; end_j++) {
                                 for(int end_k = start_k; end_k < C; end_k++) {
-                                    int sum = arr[end_i][end_j][end_k]
+                                    // Long long sum to calculate sum-sum from starting to end coords.
+                                    long long sum = arr[end_i][end_j][end_k]
                                                - (start_i ? arr[start_i-1][end_j][end_k]: 0)
                                                - (start_j ? arr[end_i][start_j-1][end_k] : 0)
                                                - (start_k ? arr[end_i][end_j][start_k-1] : 0)
@@ -60,7 +70,8 @@ int main() {
             }
         }
 
-        printf("%lld\n\n", max_sum);
+        printf("%lld\n", max_sum);
+        if(test_cases) printf("\n");
 
 
 
--- UVA-Practice/uva_10755.java
+++ UVA-Practice/uva_10755.java
@@ -15,12 +15,24 @@ class Main {
                 C = sc.nextInt();
 
                 BigInteger arr[][][] = new BigInteger[A][B][C];
-
+                int s_i = 0, s_j = 0, s_k = 0, e_i = 0, e_j = 0, e_k = 0;
+                BigInteger RSum = BigInteger.ZERO;
+                BigInteger maxVal = BigInteger.ZERO;
                 for(int i = 0; i<A; i++) {
                     for(int j = 0; j<B; j++) {
                         for(int k = 0; k<C; k++) {
                             BigInteger val;
                             val = sc.nextBigInteger();
+
+
+                            if(i == 0 && j == 0 && k == 0) {
+                                RSum = val;
+                                maxVal = val;
+                            }
+                            if(maxVal.compareTo(val) == -1) {
+                                maxVal = val;
+                            }
+
                             arr[i][j][k] = val.add(
                                     (j > 0 ? arr[i][j-1][k] : BigInteger.ZERO)).add(
                                      (k > 0 ? arr[i][j][k-1] : BigInteger.ZERO)).add(
@@ -30,9 +42,65 @@ class Main {
                                      ((i > 0 && j > 0) ? arr[i-1][j-1][k] : BigInteger.ZERO)).add(
                                      ((i > 0 && j > 0 && k > 0) ? arr[i-1][j-1][k-1] : BigInteger.ZERO));
 
+                            // As we run through the array, if the sum from start to pos is negative, reset start coords
+                                BigInteger sum = BigInteger.ZERO;
+
                         }
                     }
                 }
+
+                BigInteger prevSum = RSum;
+                for(int i = 0; i<A; i++) {
+                    for(int j = 0; j<B; j++) {
+                        for(int k = 0; k<C; k++) {
+                            System.out.printf("%d %d %d - %d %d %d\n", s_i, s_j, s_k, e_i, e_j, e_k);
+
+                            // As we run through the array, if the sum from start to pos is negative, reset start coords
+                            BigInteger sum = BigInteger.ZERO;
+
+                            sum =arr[i][j][k].subtract(
+                                    (s_i > 0 ? arr[s_i-1][j][k]: BigInteger.ZERO)).subtract(
+                                    (s_j > 0 ? arr[i][s_j-1][k] : BigInteger.ZERO)).subtract(
+                                    (s_k > 0 ? arr[i][j][s_k-1] : BigInteger.ZERO)).subtract(
+                                    (s_i > 0 && s_j > 0  && s_k > 0 ? arr[s_i-1][s_j-1][s_k-1] : BigInteger.ZERO)).add(
+                                    (s_i > 0 && s_j > 0 ? arr[s_i-1][s_j-1][k] : BigInteger.ZERO)).add(
+                                    (s_i > 0 && s_k > 0 ? arr[s_i-1][j][s_k-1] : BigInteger.ZERO)).add(
+                                    (s_j > 0 && s_k > 0 ? arr[i][s_j-1][s_k-1] : BigInteger.ZERO));
+                            System.out.println(sum + ": max_sum: " + RSum);
+                            if(sum.compareTo(prevSum) == - 1) {
+                                prevSum = sum;
+                                s_i = i;
+                                s_j = j;
+                                s_k = k;
+                            }
+                            if(sum.compareTo(RSum) == 1) {
+                                RSum = sum;
+                                e_i = i;
+                                e_j = j;
+                                e_k = k;
+                            }
+
+                        }
+                    }
+                }
+
+
+
+
+
+
+                BigInteger temp = BigInteger.ZERO;
+
+                temp =arr[e_i][e_j][e_k].subtract(
+                        (s_i > 0 ? arr[s_i-1][e_j][e_k]: BigInteger.ZERO)).subtract(
+                        (s_j > 0 ? arr[e_i][s_j-1][e_k] : BigInteger.ZERO)).subtract(
+                        (s_k > 0 ? arr[e_i][e_j][s_k-1] : BigInteger.ZERO)).subtract(
+                        (s_i > 0 && s_j > 0  && s_k > 0 ? arr[s_i-1][s_j-1][s_k-1] : BigInteger.ZERO)).add(
+                        (s_i > 0 && s_j > 0 ? arr[s_i-1][s_j-1][e_k] : BigInteger.ZERO)).add(
+                        (s_i > 0 && s_k > 0 ? arr[s_i-1][e_j][s_k-1] : BigInteger.ZERO)).add(
+                        (s_j > 0 && s_k > 0 ? arr[e_i][s_j-1][s_k-1] : BigInteger.ZERO));
+
+                System.out.println("Temporary Result: " + (temp.compareTo(maxVal) == -1 ? maxVal : temp));
 
                 BigInteger max_sum = arr[0][0][0];
                 // O(n^6)... but the max of n is 20 ~=~ 2^4, thus 2^24? 2^10 = 1024, so 1024^2? Ouch.
